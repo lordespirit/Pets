fb874dec6b48ab2908ad5ed9828930e1d275ee1a
alumne <alumne@PC04S21>
2017-06-26 13:43:12 +0200

    Test funcional

--- Pets/src/test/TestDataFile.java
+++ Pets/src/test/TestDataFile.java
@@ -28,13 +28,54 @@ public class TestDataFile {
 		}catch (Exception e) {
 			e.printStackTrace(); 
 		}
+		
+		/*
 		// Ojo en este test no funciona comparar los objetos con equals
 		// porque ?
 		Assert.assertEquals(list.get(0).toString(), results.get(0).toString());
 		Assert.assertEquals(list.get(1).toString(), results.get(1).toString());
 		Assert.assertEquals(list.get(2).toString(), results.get(2).toString());
 		Assert.assertEquals(list.get(3).toString(), results.get(3).toString());
-	
+		*/
+		
+		Canido can      = new Canido("Firulais", 40, 0.50f,0.50f);
+		Felino felino   =new Felino("Garffiel", 20, 0.20f,0.30f); 
+		Ave parrot 		= new Ave("Parro", 0.5f, 0.10f,0.15f); 
+		Roedor miki   = new Roedor("Miki", 0.2f, 0.5f,0.1f); 
+		
+		//Propietarios 
+		Person propieCan = new Person("Fredy Campino;0034656000000;fredy@poo.com;Calle Campino"); 
+		Person propieFelino = new Person("Alex  Guix;0034656000001; jordi@poo.com;Calle Guix");
+		Person propieParrot = new Person("Josep Cardona;0034656000002;josep@poo.com;Calle Cardona");
+		Person propieMiki = new Person("Marc  Font;0034656000003;marc@poo.com;Calle font");
+				
+		// iniciar el propietario a cada mascota
+		can.setPropietario(propieCan);
+		felino.setPropietario(propieFelino); 	
+		parrot.setPropietario(propieParrot); 	
+		miki.setPropietario(propieMiki); 
+		
+		
+		Assert.assertEquals(can.getNombre(), list.get(0).getNombre());
+		Assert.assertEquals(can.getAltura(), list.get(0).getAltura(),0.01);
+		Assert.assertEquals(can.getPeso(), list.get(0).getPeso(),0.01);
+		Assert.assertEquals(can.getPropietario().getFullName(), list.get(0).getPropietario().getFullName());
+		
+		Assert.assertEquals(felino.getNombre(), list.get(1).getNombre());
+		Assert.assertEquals(felino.getAltura(), list.get(1).getAltura(),0.01);
+		Assert.assertEquals(felino.getPeso(), list.get(1).getPeso(),0.01);
+		Assert.assertEquals(felino.getPropietario().getFullName(), list.get(1).getPropietario().getFullName());
+		
+		Assert.assertEquals(parrot.getNombre(), list.get(2).getNombre());
+		Assert.assertEquals(parrot.getAltura(), list.get(2).getAltura(),0.01);
+		Assert.assertEquals(parrot.getPeso(), list.get(2).getPeso(),0.01);
+		Assert.assertEquals(parrot.getPropietario().getFullName(), list.get(2).getPropietario().getFullName());
+		
+		Assert.assertEquals(miki.getNombre(), list.get(3).getNombre());
+		Assert.assertEquals(miki.getAltura(), list.get(3).getAltura(),0.01);
+		Assert.assertEquals(miki.getPeso(), list.get(3).getPeso(),0.01);
+		Assert.assertEquals(miki.getPropietario().getFullName(), list.get(3).getPropietario().getFullName());
+		
 	}
 	
 	 
